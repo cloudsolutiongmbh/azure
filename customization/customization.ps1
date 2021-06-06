b54e90c13abbbf0f35a4aaf489d458c0c0089771
cloudsolutiongmbh <84773107+cloudsolutiongmbh@users.noreply.github.com>
2021-06-06 18:51:15 +0200

    Update customization.ps1

--- customization/customization.ps1
+++ customization/customization.ps1
@@ -1,4 +1,3 @@
-#Software Installation
 $module = "Evergreen"
 if (!(Get-Module -ListAvailable -Name $module)) {
     Install-Module -Name $module -Force
@@ -7,10 +6,10 @@ else {
     Import-Module -Name $module
 }
 
-function Create-Folder($folder) {
+function Create($path) {
 
-    if (!(Test-Path -path $folder)) {
-        New-Item -ItemType directory -Path $folder -Force -ErrorAction "SilentlyContinue"
+    if (!(Test-Path -path $path)) {
+        New-Item -ItemType directory -Path $path -Force -ErrorAction "SilentlyContinue"
     }
 
 }
@@ -24,39 +23,45 @@ function Install($setup, $arguments) {
 }
 
 $folder = "C:\customization-cs"
-Create-Folder -path $folder
-
+Create -path $folder
 
 # Office 365
 Write-Host "---------- Office 365 ----------"
 # Office 365 Object Configuration - download from CS Github
+$o365_path = $folder + "\o365"
+
 $o365 = @{
     url   = "https://github.com/cloudsolutiongmbh/azure/raw/main/customization/o365setup.zip"
-    path  = $folder + "\o365"
-    setup = $o365.path + "\setup.exe"
-    arg   = "/configure " + $o365.path + "\config.xml"
-    zip   = $o365.path + "\o365setup.zip"
+    zip   = $o365_path + "\o365setup.zip"
+    setup = $o365_path + "\setup.exe"
+    arg   = "/configure " + $o365_path + "\config.xml"
 }
+
 # Office365 Installation
-Create-Folder -path $o365.path
+Create -path $o365_path
 Download -url $o365.url -outfile $o365.zip
-Expand-Archive -LiteralPath $o365.zip -DestinationPath $o365.path
-Install -setup $o365.setup -arguments $o365.arg
+Expand-Archive -LiteralPath $o365.zip -DestinationPath $o365_path
+if (Test-Path -Path $o365.setup) {
+    Install -setup $o365.setup -arguments $o365.arg
+}
 
 
 #OneDrive Machine Installation
 Write-Host "---------- OneDrive Machine Installation ----------"
 # Onedrive Object Configuration - download via Evergreen
+$onedrive_path = $folder + "\onedrive"
+
 $onedrive = @{
     url   = (Get-EvergreenApp -Name "MicrosoftOneDrive" | Where-Object { $_.Architecture -eq "x86" -and $_.Ring -eq "Enterprise" -and $_.Type -eq "exe" }).Uri
-    path  = folder + "\onedrive"
-    setup = $onedrive.path + "\OneDriveSetup.exe"
+    setup = $onedrive_path + "\OneDriveSetup.exe"
     arg   = "/silent /allusers"
 }
 # Onedrive Installation
-Create-Folder -path $onedrive.path
+Create -path $onedrive_path
 Download -url $onedrive.url -outfile $onedrive.setup
-Install -setup $onedrive.setup -arguments $onedrive.arg
+if (Test-Path -Path $onedrive.setup) {
+    Install -setup $onedrive.setup -arguments $onedrive.arg
+}
 
 #FSLogix Delete Keys in Script
 Write-Host "---------- CleanUp FsLogix ----------"
@@ -76,16 +81,16 @@ foreach ($key in $fslogix_keys) {
 # MS Teams FW Rule
 # based on https://msendpointmgr.com/2020/03/29/managing-microsoft-teams-firewall-requirements-with-intune/
 $teams = "C:\Program Files (x86)\Microsoft\teams\current\teams.exe"
-if(Test-Path -path $teams){
-    if(Get-NetFirewallApplicationFilter -Program $teams -ErrorAction SilentlyContinue){
+if (Test-Path -path $teams) {
+    if (Get-NetFirewallApplicationFilter -Program $teams -ErrorAction SilentlyContinue) {
         Get-NetFirewallApplicationFilter -Program $teams -ErrorAction SilentlyContinue | Remove-NetFirewallRule -ErrorAction SilentlyContinue
     }
     New-NetFirewallRule -DisplayName "MS Teams" -Direction Inbound -Profile Domain -Program $teams -Action Allow -Protocol Any
-    New-NetFirewallRule -DisplayName "MS Teams" -Direction Inbound -Profile Public,Private -Program $teams -Action Block -Protocol Any
+    New-NetFirewallRule -DisplayName "MS Teams" -Direction Inbound -Profile Public, Private -Program $teams -Action Block -Protocol Any
 }
 
 #CleanUp
 $paths = @($folder, "C:\optimize", "C:\teams", "C:\temp")
-foreach ($path in $paths){
-   if(Test-Path -Path $path){Remove-Item -Path $path -Recurse -Force}
+foreach ($path in $paths) {
+    if (Test-Path -Path $path) { Remove-Item -Path $path -Recurse -Force }
 }
